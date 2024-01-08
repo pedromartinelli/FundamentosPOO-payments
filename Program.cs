e3c1cc6e95ffb43ada1954763b415d9e604fe345
Pedro Martinelli <p.martinellinunes@gmail.com>
2024-01-08 15:01:13 -0300

    métodos construtores

--- Program.cs
+++ Program.cs
@@ -6,32 +6,37 @@ namespace Payments
     {
         static void Main(string[] args)
         {
-            var payment = new BoletoPayment();
-            payment.Pay("123");
+            var payment = new BoletoPayment(DateTime.Now.AddMonths(1));
+            payment.Pay("342131");
         }
     }
 
-    /* Modificadores de acesso: 
+    /*  
+        Modificadores de acesso: 
         private: acessado apenas dentro da classe
         protected: acessado dentro da classe e pelos filhos
         internal: acessado dentro da classe, pelos filhos e no namespace
         public: acessado em toda a aplicação 
-     */
+    */
 
     // Encapsulamento
     public class Payment
     {
+
+        // Método construtor
+        public Payment(DateTime expireAt)
+        {
+
+        }
+
         // Propriedades
         public DateTime ExpireAt { get; set; }
         public Address BillingAddress { get; set; }
 
-        // Métodos
-        public virtual void Pay(string number) 
-        {
-            Console.WriteLine("Pagar");
-        }
+        // Métodos     // Assinatura do método
+        public virtual void Pay(string number) { }
 
-        // Sobrecarga de método
+        // Sobrecarga de método        // Assinatura do método
         public virtual void Pay(string number, DateTime paymentDate) { }
 
         // Polimorfismo
@@ -44,20 +49,33 @@ namespace Payments
     // Herança
     class BoletoPayment : Payment
     {
+
+        // Método construtor
+        public BoletoPayment(DateTime expireAt) : base(expireAt)
+        {
+            Console.WriteLine($"\nIniciando pagamento com boleto... \n*Vencimento: {expireAt}\n");
+        }
+
+
         public string BoletoNumber { get; set; }
 
         // Polimorfismo
         // Sobrescrita de métodos
         public override void Pay(string number) 
         {
-            base.Pay(number);
-            Console.WriteLine($"Pagar com boleto de numero {number}");
+            Console.WriteLine($"Pagar com boleto de numero {number}\n");
         }
-
     }
 
     class CreditCardPayment : Payment 
     {
+
+        // Método construtor
+        public CreditCardPayment(DateTime expireAt) : base(expireAt)
+        {
+
+        }
+
         public string CreditCardNumber { get; set; }
 
         // Polimorfismo
@@ -78,5 +96,11 @@ namespace Payments
     {
         public string ZipCode { get; set; }
         public string Street { get; set; }
+    }
+
+    public enum PaymentType
+    {
+        Boleto = 1,
+        CreditCard = 2
     }
 }
